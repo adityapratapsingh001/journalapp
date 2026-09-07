1c5def70cf32fdf9e35cfd42b879f75504ef0804
adityathakur <adityathakur6100@gmail.com>
2026-09-07 20:06:38 +0530

    Initial commit

---
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -0,0 +1,33 @@
+package net.engineeringdigest.journalApp.entity;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Document(collection = "journal_entries")
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor    **** ??    We use @DATA for all
+//@EqualsAndHashCode
+//@ToString
+//@Builder
+//@Slf4j
+
+@Data
+@NoArgsConstructor
+public class JournalEntry {
+    @Id
+    private ObjectId id;
+    @NonNull
+    private String title;
+    private String content;
+    private LocalDateTime date;
+
+}
+
